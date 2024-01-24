65bcd153cbbc0b2ac41b47a4eb7e2f503c6466a1
IliaLataria01 <ilataria@solvd.com>
2024-01-24 14:45:32 +0400

    Dynamic multiplication table

--- practice/main.go
+++ practice/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
@@ -37,19 +39,36 @@ func main() {
 	//	fmt.Println("There was no error, n is:", n)
 	//} else if err != nil {
 	//	fmt.Println("There was an error:", err)
-	//}
+	//}1
 	//
 	//fmt.Printf("Because of Shadowing values are n: %d, err: %v", n, err)
+	var (
+		number int
+		err    error
+	)
 
-	i := 142
-	switch {
-	case i > 100:
-		fmt.Print("big ")
-		fallthrough
-	case i > 100:
-		fmt.Print("positive ")
-		fallthrough
-	default:
-		fmt.Print("number")
+	if n := os.Args; len(n) < 1 {
+		fmt.Println("There was no argument")
+	} else if err != nil {
+		fmt.Println("There was an error")
+	} else {
+		number, err = strconv.Atoi(n[1])
+
+	}
+
+	fmt.Println(len(os.Args))
+	fmt.Printf("%s", "X")
+	for i := 0; i <= number; i++ {
+		fmt.Printf("%10d", i)
+	}
+	fmt.Println()
+	for i := 0; i <= number; i++ {
+		fmt.Printf("%d", i)
+		for j := 0; j <= number; j++ {
+			fmt.Printf("%10d", i*j)
+
+		}
+		fmt.Println()
+
 	}
 }
