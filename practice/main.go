e3ef6e1388553c6768a070843a7ca0547bce09de
IliaLataria01 <latariailia6@gmail.com>
2024-02-04 01:50:13 +0400

    Another Day learning go: Append and Sort numbers

--- practice/main.go
+++ practice/main.go
@@ -2,32 +2,29 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
+	"slices"
+	"strconv"
 )
 
 func main() {
 
-	var (
-		pizza_toppings   []string
-		departure_times  []time.Time
-		graduation_years []int
-		states_of_lights []bool
-	)
+	arg := os.Args
 
-	pizza_toppings = append(pizza_toppings, "pepperoni", "onions", "extra cheese")
+	if len(arg) <= 1 {
+		fmt.Println("provide a few numbers")
+	} else {
+		var lst []int
+		arr := arg[1:]
 
-	now := time.Now()
-	departure_times = append(departure_times,
-		now,
-		now.Add(time.Hour*24), // 24 hours after `now`
-		now.Add(time.Hour*48)) // 48 hours after `now`
-
-	graduation_years = append(graduation_years, 1998, 2005, 2018)
-
-	states_of_lights = append(states_of_lights, true, false, true)
-
-	fmt.Printf("pizza       : %s\n", pizza_toppings)
-	fmt.Printf("\ngraduations : %d\n", graduation_years)
-	fmt.Printf("\ndepartures  : %s\n", departure_times)
-	fmt.Printf("\nlights      : %t\n", states_of_lights)
+		for i := 0; i < len(arr); i++ {
+			num, err := strconv.Atoi(arr[i])
+			if err != nil {
+				continue
+			}
+			lst = append(lst, num)
+		}
+		slices.Sort(lst)
+		fmt.Println(lst)
+	}
 }
