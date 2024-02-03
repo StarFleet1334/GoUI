098a8a67382c29f20b7bd5a3ba60fe3a14c33f46
IliaLataria01 <latariailia6@gmail.com>
2024-02-04 02:14:37 +0400

    Another Day learning go: added Housing prices and Housing prices with Averages

--- practice/main.go
+++ practice/main.go
@@ -2,29 +2,50 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"slices"
 	"strconv"
 )
 
 func main() {
 
-	arg := os.Args
+	header := []string{"Location", "Size", "Beds", "Baths", "Price"}
+	type placeholder [5]string
 
-	if len(arg) <= 1 {
-		fmt.Println("provide a few numbers")
-	} else {
-		var lst []int
-		arr := arg[1:]
+	var data []placeholder
 
-		for i := 0; i < len(arr); i++ {
-			num, err := strconv.Atoi(arr[i])
-			if err != nil {
-				continue
-			}
-			lst = append(lst, num)
-		}
-		slices.Sort(lst)
-		fmt.Println(lst)
+	data = append(data, placeholder{
+		"New York", "100", "2", "1", "100000",
+	}, placeholder{
+		"New York", "150", "3", "2", "200000",
+	}, placeholder{
+		"Paris", "200", "4", "3", "400000",
+	}, placeholder{
+		"Istanbul", "500", "10", "5", "100000",
+	})
+
+	for _, item := range header {
+		fmt.Printf("%-15s", item)
 	}
+	fmt.Println("\n===========================================================================")
+	var result [5]int
+
+	for _, item := range data {
+		for i, insideItem := range item {
+			if i > 0 {
+				num, _ := strconv.Atoi(insideItem)
+				result[i] = result[i] + (num)
+			}
+			fmt.Printf("%-15s", insideItem)
+		}
+		fmt.Println("\n")
+	}
+	fmt.Println("\n===========================================================================")
+	for i, item := range result {
+		if i == 0 {
+			fmt.Printf("%-15s", "")
+
+		} else {
+			fmt.Printf("%-15.2f", float64(item)/float64(len(data[0])))
+		}
+	}
+
 }
