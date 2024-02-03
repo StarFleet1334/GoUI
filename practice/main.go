da4cd93680cfec4fd181b7fcb3a5e38becba0181
IliaLataria01 <latariailia6@gmail.com>
2024-02-03 22:20:17 +0400

    Another Day learning go: Assigning slice literals to the slices

--- practice/main.go
+++ practice/main.go
@@ -13,20 +13,20 @@ func main() {
 		ratios    []float64
 		alives    []bool
 	)
-	names = []string{}
-	distances = []int{}
-	data = []uint8{}
-	ratios = []float64{}
-	alives = []bool{}
+	names = []string{"Saba", "Luka", "Gio"}
+	distances = []int{1, 2, 3, 4, 5}
+	data = []uint8{'I', 'N', 'A', 'N', 'C'}
+	ratios = []float64{1.1, 1.2}
+	alives = []bool{true, false, false, false}
 
-	fmt.Printf("names    : %#v %d %t \n", names, len(names), names == nil)
+	fmt.Printf("names    : %T %d %t \n", names, len(names), names == nil)
 
-	fmt.Printf("distances    :  %#v %d %t \n", distances, len(distances), data == nil)
+	fmt.Printf("distances    :  %T %d %t \n", distances, len(distances), data == nil)
 
-	fmt.Printf("data    :  %#v %d %t \n", data, len(data), data == nil)
+	fmt.Printf("data    :  %T %d %t \n", data, len(data), data == nil)
 
-	fmt.Printf("ratios    :  %#v %d %t \n", ratios, len(ratios), ratios == nil)
+	fmt.Printf("ratios    :  %T %d %t \n", ratios, len(ratios), ratios == nil)
 
-	fmt.Printf("alives    :  %#v %d %t", alives, len(alives), alives == nil)
+	fmt.Printf("alives    :  %T %d %t", alives, len(alives), alives == nil)
 
 }
