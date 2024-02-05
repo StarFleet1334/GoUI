6cedc7a20511091d28bcd375a3b2248267aac1c7
IliaLataria01 <latariailia6@gmail.com>
2024-02-05 07:54:49 +0400

    Another Day learning go: sort the backing array

--- practice/main.go
+++ practice/main.go
@@ -2,55 +2,22 @@ package main
 
 import (
 	"fmt"
-	"github.com/inancgumus/prettyslice"
+	"slices"
 )
 
 type collection []string
 
 func main() {
-	//// DON'T TOUCH THE FOLLOWING CODE
-	//nums := []int{56, 89, 15, 25, 30, 50}
-	//
-	//// ----------------------------------------
-	//// ONLY ADD YOUR CODE HERE
-	////
-	//// Ensure that nums slice never changes even though
-	//// the mine slice changes.
-	//mine := nums
-	//// ----------------------------------------
-	//
-	//// DON'T TOUCH THE FOLLOWING CODE
-	////
-	//// This code changes the elements of the nums
-	//// slice.
-	////
-	//mine[0], mine[1], mine[2] = -50, -100, -150
-	//
-	//fmt.Println("Mine         :", mine)
-	//fmt.Println("Original nums:", nums[:3])
-	prettyslice.MaxPerLine = 6
-	// DON'T TOUCH THE FOLLOWING CODE
-	nums := []int{56, 89, 15, 25, 30, 50}
+	items := []string{
+		"pacman", "mario", "tetris", "doom", "galaga", "frogger",
+		"asteroids", "simcity", "metroid", "defender", "rayman",
+		"tempest", "ultima",
+	}
 
-	// ----------------------------------------
-	// ONLY ADD YOUR CODE HERE
-	//
-	// Ensure that nums slice never changes even though
-	// the mine slice changes.
+	fmt.Println("Original:", items)
+	// ADD YOUR CODE HERE
+	slices.Sort(items[(len(items)/2)-1 : (len(items)/2)+2])
+	fmt.Println()
+	fmt.Println("Sorted  :", items)
 
-	// Solution #1
-	//mine := [3]int{}
-	//mine = [3]int(append(mine[:], nums[:3]...))
-
-	// Solution #2
-	mine := append([]int(nil), nums[:3]...)
-
-	// ----------------------------------------
-	// DON'T TOUCH THE FOLLOWING CODE
-	//
-	// This code changes the elements of the nums
-	// slice.
-	//
-	fmt.Println("Mine         :", mine)
-	fmt.Println("Original nums:", nums[:3])
 }
